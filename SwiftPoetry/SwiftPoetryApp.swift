0586bc71d5da6747fdb58b12b9793b0c7a096613
Dominic Campbell <dom_campbell@hotmail.com>
2024-10-20 18:46:45 +0100

    Initial Commit

---
+++ SwiftPoetry/SwiftPoetryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftPoetryApp.swift
+//  SwiftPoetry
+//
+//  Created by Dominic Campbell on 20/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftPoetryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
